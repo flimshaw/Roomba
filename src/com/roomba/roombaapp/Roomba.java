ff10dd1fc917ea77d41e7fadc3060bc20b5ffcdf
Charlie Hoey <charlie.hoey@gmail.com>
2010-02-06 21:22:24 -0500

    initial java project files

---
+++ src/com/roomba/roombaapp/Roomba.java
@@ -0,0 +1,13 @@
+package com.roomba.roombaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Roomba extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
